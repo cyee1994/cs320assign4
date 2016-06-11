f4ec3dba458d09f5f943214cb42d489db9242f3a
Christopher Yee <christopheryee@att.net>
2016-06-10 23:57:14 -0700

    Uploaded updated prog4_3.sh

--- prog4_3.sh
+++ prog4_3.sh
@@ -1,1 +1,23 @@
 #!/bin/bash
+echo Assignment 4-3.sh, Christopher Yee, christopheryee@att.net
+offset=$1
+
+for prog3 in $(find . -name 'prog4_3.c')
+	do
+	 
+	 gcc $prog3
+		
+	done
+
+
+for encFiles in *.enc
+
+	do
+	
+	  echo $encFiles|./a.out "$offset"> "$encFiles.dec"
+
+	done
+
+
+
+
